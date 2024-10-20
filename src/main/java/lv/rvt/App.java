0e28ae56e3f3621d0280165349ce35b685120bcd
VadimsMalickis <vadims.malickis@rvt.lv>
2024-10-20 21:25:22 +0300

    init config

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,21 @@
 package lv.rvt;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        // Make your Hello World programm!
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, World!");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello World App");
+        primaryStage.show();
     }
 }
